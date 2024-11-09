1f4a6bcd9ded0e2e4b946c342a33e1d5a32de88b
dungnm <manhdung1682003@gmail.com>
2024-11-09 16:15:58 +0700

    add card gg

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,8 @@ import AppRouter from "./navigation/AppRouter";
 import { AuthProvider } from "./service/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GOOGLE_CLIENT_ID } from "./constants/Constants";
-import ReactGA from 'react-ga4';
-import React from 'react';  
-
-const MEASUREMENT_ID = "G-JLYH0CB9NF";
 
 function App() {
-  ReactGA.initialize([
-    {
-      trackingId: MEASUREMENT_ID
-    }
-  ])
-
-  React.useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: window.location.pathname
-    })
-  }, [])
-
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <AuthProvider>
